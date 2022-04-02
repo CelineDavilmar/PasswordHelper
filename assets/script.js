2b8c4c69dcdbcc2e240311b1ae3a3f68fc6f18fa
Celine Davilmar <celinedavilmar@Celines-MacBook-Pro.local>
2022-04-02 17:29:13 -0400

    added options 2 and 3

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,7 @@
 // Assignment code here
-
-  //userChoices();
-
-
 function generatePassword() {
-  //Collecting userInut
+
+  //Collecting userInut for desired password length
     let userInput = window.prompt("Please enter the desired length of the password. Only input a number between 8 - 128");
 
     for (let i = 0; i < userInput.length; i++){
@@ -16,12 +13,15 @@ function generatePassword() {
          generatePassword();
       }
     }
-    CharacterOption();
+    CharacterOption1();
+    CharacterOption2();
+    CharacterOption3();
 
-    function CharacterOption() {
-    let charOption = window.prompt("Do you want to include special characters? Input yes or no");
+    //Collecting userInut for special characters option
+    function CharacterOption1() {
+      let charOption1 = window.prompt("Do you want to include special characters? Input yes or no");
     
-          switch (charOption) {
+          switch (charOption1) {
             case "yes" || "Yes":
             window.alert("Okay will generate password that is " + userInput + " numbers long with special characters included");
               break;
@@ -30,10 +30,43 @@ function generatePassword() {
               break;
             default :
             window.alert("Please only input yes or no");
-            CharacterOption();
+            CharacterOption1();
           }
         };
+    
+    //Collecting userInut for upper case & lowercase characters option
+    function CharacterOption2() {
+      let charOption2 = window.prompt("Do you want to include upper case & lowercase characters? Input yes or no");
+          
+          switch (charOption2) {
+            case "yes" || "Yes":
+            window.alert("Okay will generate password that is " + userInput + " numbers long with upper case & lowercase characters included");
+              break;
+            case "no" || "No":
+            window.alert("Okay will not include upper case & lowercase characters and will make the password " + userInput + " numbers long");
+              break;
+            default :
+            window.alert("Please only input yes or no");
+            CharacterOption2();
+          }
+    };
 
+    //Collecting userInut for numeric characters option
+    function CharacterOption3() {
+      let charOption3 = window.prompt("Do you want to include numeric characters? Input yes or no");
+          
+          switch (charOption3) {
+            case "yes" || "Yes":
+            window.alert("Okay will generate password that is " + userInput + " numbers long with numeric characters included");
+              break;
+            case "no" || "No":
+            window.alert("Okay will not include numeric characters and will make the password " + userInput + " numbers long");
+              break;
+            default :
+            window.alert("Please only input yes or no");
+            CharacterOption3();
+          }
+    };
           for (let i = 0; i < userInput; i++){
             var randomNumber = Math.floor(Math.random() * chars.length);
             password += chars.substring(randomNumber, randomNumber +1);
